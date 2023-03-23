8cdfb12e7a3c694236625c3e9f76e4e77f7b0f4f
divyesh <divyesh@bbd.co.za>
2023-03-23 15:06:07 +0530

    intial commit for project microservice

---
+++ Backend/fl-project-service/fl-project-service/src/main/java/com/fl/project/FlProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fl.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlProjectServiceApplication.class, args);
+	}
+
+}
