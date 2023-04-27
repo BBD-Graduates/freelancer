ef5e93f2292da95befda63b9f6850d3c53841db3
Nom0210 <125949491+Nom0210@users.noreply.github.com>
2023-04-27 16:22:09 +0530

    Done with Insert Update Delete Currently in the middle of display (#8)
    * Done with Insert Update Delete Currently in the middle of display
    
    * CRUD
    
    * path variable added

---
+++ Backend/fl-bid-service/fl-bid-service/src/main/java/com/fl/bid/controller/BidController.java
@@ -0,0 +1,61 @@
+package com.fl.bid.controller;
+import com.fl.bid.model.FlResponse;
+import com.fl.bid.model.request.BidRequest;
+import com.fl.bid.model.response.Bid;
+import com.fl.bid.service.serviceInterface.BidService;
+import com.fl.bid.util.FlResponseUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import static com.fl.bid.config.Constant.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bids")
+@RequiredArgsConstructor
+public class BidController {
+    private final BidService bidService;
+    private final FlResponseUtil flResponseUtil;
+
+    @PostMapping
+    public ResponseEntity<FlResponse<String>> addBid(@Valid @RequestBody BidRequest bidRequest) {
+        try {
+            return flResponseUtil.getResponseEntity(HttpStatus.OK, bidService.insertBid(bidRequest), String.format("%s " + INSERTED_SUCCESSFULLY, BID));
+        } catch (Exception e) {
+            return flResponseUtil.getResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, null, String.format("%s " + INSERTION_FAILED, BID));
+        }
+    }
+    @DeleteMapping("/{bidId}")
+    public ResponseEntity<FlResponse<String>> deleteBid(@PathVariable("bidId") int bidId) {
+        try {
+            return flResponseUtil.getResponseEntity(HttpStatus.OK, bidService.deleteBid(bidId), String.format("%s" + DELETED_SUCCESSFULLY, BID));
+        } catch (Exception e) {
+            return flResponseUtil.getResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, null, String.format("%s " + DELETION_FAILED, BID));
+        }
+    }
+    @PutMapping("/{bidId}")
+    public ResponseEntity<FlResponse<String>> updateBid(@PathVariable("bidId") Integer bidId, @Valid @RequestBody BidRequest bidRequest) {
+        try {
+            return flResponseUtil.getResponseEntity(HttpStatus.OK, bidService.updateBid(bidRequest, bidId), String.format("%s" + UPDATED_SUCCESSFULLY, BID));
+        } catch (Exception e) {
+            return flResponseUtil.getResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, null, String.format("%s " + UPDATION_FAILED, BID));
+        }
+    }
+    @GetMapping
+    public ResponseEntity<FlResponse<List<Bid>>> getBids(
+            @RequestParam(defaultValue = "0", required = false, name = "bidId") Integer bidId,
+            @RequestParam(defaultValue = "0", required = false, name = "projectId") Integer projectId,
+            @RequestParam(defaultValue = "0", required = false, name = "freelancerId") Integer freelancerId){
+        try {
+            return flResponseUtil.getResponseEntity(HttpStatus.OK, bidService.getBids(bidId,projectId,freelancerId),String.format("%s" + FETCHED_SUCCESSFULLY, BID));
+        } catch (Exception e) {
+            return flResponseUtil.getResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, null, String.format("%s " + NO_RECORD_FOUND));
+        }
+    }
+}
+
+
+
---
+++ Backend/fl-bid-service/fl-bid-service/src/main/java/com/fl/bid/service/BidServiceImpl.java
@@ -0,0 +1,82 @@
+package com.fl.bid.service;
+import com.fl.bid.config.Constant;
+import com.fl.bid.model.request.BidRequest;
+import com.fl.bid.model.response.Bid;
+import com.fl.bid.repository.DbQueries;
+import com.fl.bid.service.serviceInterface.BidService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BidServiceImpl implements BidService {
+    private final DbQueries dbQueries;
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public String insertBid(BidRequest bidRequest) {
+        try {
+            int insertStatus = jdbcTemplate.update(dbQueries.getAddBid(),
+                    bidRequest.getProjectId(), bidRequest.getFreelancerId(), bidRequest.getAmount(), bidRequest.getDescription());
+            if (insertStatus > 0) {
+                return Constant.INSERTED_SUCCESSFULLY;
+            } else {
+                return Constant.CANT_PROCESS_REQUEST;
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public String deleteBid(int bidId) {
+        try {
+            int deleteStatus = jdbcTemplate.update(dbQueries.getRemoveBid(), bidId);
+            if (deleteStatus > 0) {
+                return Constant.DELETED_SUCCESSFULLY;
+            } else {
+                return Constant.DELETION_FAILED;
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public String updateBid(BidRequest bidRequest, int bidId) {
+        try {
+            int updateStatus = jdbcTemplate.update(dbQueries.getUpdateBid(), bidRequest.getAmount(), bidRequest.getDescription(), bidId);
+            if (updateStatus > 0) {
+                return Constant.UPDATED_SUCCESSFULLY;
+            } else {
+                return Constant.UPDATION_FAILED;
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public List<Bid> getBids(Integer bidId, Integer projectId, Integer freelancerId) {
+        try {
+            if (!bidId.equals(0)) {
+                return jdbcTemplate.query(dbQueries.getBidByBidId(),
+                        BeanPropertyRowMapper.newInstance(Bid.class), bidId);
+            }else if(!projectId.equals(0)){
+                return jdbcTemplate.query(dbQueries.getBidByProjectId(), BeanPropertyRowMapper.newInstance(Bid.class),  projectId);
+            }else if(!freelancerId.equals(0)){
+                return jdbcTemplate.query(dbQueries.getBidByFreelancerId(), BeanPropertyRowMapper.newInstance(Bid.class), freelancerId);
+            }
+            else {
+                return jdbcTemplate.query(dbQueries.getAllBid(), BeanPropertyRowMapper.newInstance(Bid.class));
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
+
+
+
