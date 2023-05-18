8ce71dcad79c6e20cfd85f062d565d166ba54251
Nom0210 <125949491+Nom0210@users.noreply.github.com>
2023-05-18 19:10:23 +0530

    ProjectList ProjectDetail/Biding (#24)

--- frontend/fl/src/app/config.ts
+++ frontend/fl/src/app/config.ts
@@ -1,12 +1,15 @@
 export const config = {
-    apiGatewayUrl: 'http://localhost:8085/',
-    projectService: '',
-    skillApi: {
-        getCategorySkills: 'http://localhost:8082/categories'
-    },
-    projectApi: {
-        getProject: 'http://localhost:8083/projects',
-        getProjectByProjectId: 'http://localhost:8083/projects?projectId='
-    },
+  apiGatewayUrl: 'http://localhost:8085/',
+  projectService: '',
+  skillApi: {
+      getCategorySkills: 'http://localhost:8082/categories'
+  },
+  projectApi: {
+      getProject: 'http://localhost:8083/projects?ProjectId=0',
+      getProjectByProjectId: 'http://localhost:8083/projects?ProjectId='
+  },
+  BidApi:{
+    insertBid: "http://localhost:8084/bids"
+  }
 
 }
