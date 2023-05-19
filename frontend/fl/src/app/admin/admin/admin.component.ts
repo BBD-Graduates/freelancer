db1ba63295badc411c2974845457f7ee8f8c3f4c
divyesh <divyesh@bbd.co.za>
2023-05-19 15:59:27 +0530

    add admin panel

--- frontend/fl/src/app/admin/admin/admin.component.ts
+++ frontend/fl/src/app/admin/admin/admin.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-  showSidebar = false;
+  showSidebar = true;
 
   toggleSidebar() {
     this.showSidebar = !this.showSidebar;
--- frontend/fl/src/app/config.ts
+++ frontend/fl/src/app/config.ts
@@ -2,7 +2,8 @@ export const config = {
   apiGatewayUrl: 'http://localhost:8085/',
   projectService: '',
   skillApi: {
-      getCategorySkills: 'http://localhost:8082/categories'
+      getCategorySkills: 'http://localhost:8082/categories',
+      getSkills: 'http://localhost:8082/skills',
   },
   projectApi: {
       getProject: 'http://localhost:8083/projects?ProjectId=0',
