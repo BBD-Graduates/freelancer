9aca9ff47992f907e2b7c456485c581d27d4a2d0
Nom0210 <125949491+Nom0210@users.noreply.github.com>
2023-06-05 15:10:20 +0530

    posting project and bidding on project (#51)

--- frontend/fl/src/app/user/components/project/project-details/project-details.component.ts
+++ frontend/fl/src/app/user/components/project/project-details/project-details.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { config } from 'src/app/config';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserapiService } from 'src/app/user/service/user-api.service';
 
 
 
@@ -19,7 +20,7 @@ export class ProjectDetailsComponent implements OnInit{
   isAllowBid:boolean=true;
   insertBid = new FormGroup({
     projectId : new FormControl(this.route.snapshot.paramMap.get('projectId')),
-    freelancerId : new FormControl(1,Validators.required),
+    freelancerId : new FormControl(localStorage.getItem('userId'),Validators.required),
     amount : new FormControl('',Validators.required),
     description : new FormControl('',Validators.required),
     deliveryDays : new FormControl('',Validators.required),
@@ -27,7 +28,7 @@ export class ProjectDetailsComponent implements OnInit{
 })
   locationData:any=[];
   http: any;
-  constructor (private _httpClient:HttpClient,private route:ActivatedRoute){
+  constructor (private _httpClient:HttpClient,private route:ActivatedRoute,private userService:UserapiService){
   }
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('projectId');
@@ -40,22 +41,16 @@ export class ProjectDetailsComponent implements OnInit{
   })
 }
 saveBid(data: any) {
+  return this._httpClient.post(config.BidApi.insertBid, data);
+}
+collectBid() {
   console.log(this.insertBid.value);
-  this._httpClient.post(config.BidApi.insertBid,data).subscribe((response:any)=>{
-    this.data = response;
-
+  this.saveBid(this.insertBid.value).subscribe((response) => {
     this.alert = true;
     this.insertBid.reset();
-  })
-  // return this._httpClient.post(config.BidApi.insertBid, data);
+  });
 }
-// collectBid() {
-//   console.log(this.insertBid.value);
-//   this.saveBid(this.insertBid.value).subscribe((response) => {
-//     this.alert = true;
-//     this.insertBid.reset();
-//   });
-// }
+
 closeAlert() {
   this.alert = false;
 }
