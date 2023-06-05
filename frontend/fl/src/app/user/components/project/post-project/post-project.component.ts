e0b80bca593e99411ce0a329a77ff11351afe85c
Nom0210 <125949491+Nom0210@users.noreply.github.com>
2023-06-05 12:34:20 +0530

    posting project with skill alert message (#50)

--- frontend/fl/src/app/user/components/project/post-project/post-project.component.ts
+++ frontend/fl/src/app/user/components/project/post-project/post-project.component.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { config } from 'src/app/config';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SkillApiService } from 'src/app/user/service/skill-api.service';
+import { ProjectApiService } from 'src/app/user/service/project-api.service';
+import { ProjectModel } from 'src/app/shared/model/projectModel';
 
 @Component({
   selector: 'fl-post-project',
@@ -17,34 +18,58 @@ export class PostProjectComponent {
   alert: boolean = false
   skillName: any = [];
   insertProject = new FormGroup({
-    projectName: new FormControl('', Validators.required),
-    projectDescription: new FormControl('', Validators.required),
-    isConfidential: new FormControl('', Validators.required),
-    bidStartDate: new FormControl('', Validators.required),
-    bidEndDate: new FormControl('', Validators.required),
-    minPrice: new FormControl('', Validators.required),
-    maxPrice: new FormControl('', Validators.required),
-    status: new FormControl('', Validators.required)
+    projectName: new FormControl('', [Validators.required]),
+    projectDescription: new FormControl('', [Validators.required]),
+    isConfidential: new FormControl('', [Validators.required]),
+    bidStartDate: new FormControl('', [Validators.required]),
+    bidEndDate: new FormControl('', [Validators.required]),
+    minPrice: new FormControl('', [Validators.required]),
+    maxPrice: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required]),
 
-  })
+  });
   skillList: any;
-  constructor(private _httpClient: HttpClient, private route: ActivatedRoute, private skillAPi: SkillApiService) { 
+  errorMessage: string;
+  constructor(private _httpClient: HttpClient, private route: ActivatedRoute, private skillAPi: SkillApiService,private projectApi:ProjectApiService) {
     this.loadSkills();
+    this.errorMessage='';
 
   }
-  
-  saveProject(projectdata: any) {
-    return this._httpClient.post(config.projectApi.insertProject, projectdata);
-  }
+
   collectProject() {
-    console.log(this.insertProject.value);
-    this.saveProject(this.insertProject.value).subscribe((response) => {
-      this.alert = true;
-      this.insertProject.reset();
-    });
+    if (this.selectedOptions.length<=5){
+
+      const projectData: ProjectModel = {
+        projectName: this.insertProject.value.projectName ?? '',
+        projectDescription: this.insertProject.value.projectDescription ?? '',
+        isConfidential: this.insertProject.value.isConfidential === 'true',
+        bidStartDate: new Date(this.insertProject.value.bidStartDate ?? ''),
+        bidEndDate: new Date(this.insertProject.value.bidEndDate ?? ''),
+        minPrice: Number(this.insertProject.value.minPrice ?? ''),
+        maxPrice: Number (this.insertProject.value.maxPrice ?? ''),
+        status: this.insertProject.value.status ?? '',
+
+      };
+
+      this.projectApi.postProject(projectData)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.alert = true;
+            this.insertProject.reset();
+          },
+          (error) => {
+            console.error('Error calling postProject service:', error);
+          }
+        );
+    }else{
+      this.errorMessage = "You can select a maximum of 5 options.";
+    }
+
   }
+
   closeAlert() {
-    this.alert = false;
+    this.errorMessage = '';
   }
 
   options:any = [];
