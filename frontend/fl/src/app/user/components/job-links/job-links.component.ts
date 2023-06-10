270c13eb28ce0fef4148f9772a4838783b2aa7fe
divyesh <divyesh@bbd.co.za>
2023-06-10 18:59:55 +0530

    api url change to ec2 instance and changes acoording to it

--- frontend/fl/src/app/user/components/job-links/job-links.component.ts
+++ frontend/fl/src/app/user/components/job-links/job-links.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { config } from 'src/app/config';
+import { SkillApiService } from '../../service/skill-api.service';
 
 @Component({
   selector: 'fl-job-links',
@@ -9,16 +10,17 @@ import { config } from 'src/app/config';
 })
 export class JobLinksComponent {
   menuData: any;
-  constructor(private _httpClient: HttpClient) {}
+  constructor(private skillService: SkillApiService) {}
 
   sessionStatus = localStorage.getItem('userEmail') == null;
 
   ngOnInit(): void {
-    this._httpClient.get(config.skillApi.getCategorySkills).subscribe((res) => {
-      this.menuData = res;
-    });
+    this.getcategory();
   }
   ngOnChange(): void {
     this.sessionStatus = localStorage.getItem('userEmail') == null;
+  }
+  async getcategory(){
+    this.menuData = await this.skillService.getCategory({});
   }
 }
