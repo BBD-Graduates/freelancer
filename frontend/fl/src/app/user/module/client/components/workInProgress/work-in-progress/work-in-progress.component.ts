db186fea22e19160b51bb4cb947f1370def89914
divyesh <divyesh@bbd.co.za>
2023-06-19 12:22:30 +0530

    when click on completed button status changes to completed

--- frontend/fl/src/app/user/module/client/components/workInProgress/work-in-progress/work-in-progress.component.ts
+++ frontend/fl/src/app/user/module/client/components/workInProgress/work-in-progress/work-in-progress.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectStatus } from 'src/app/enums/projectStatusEnums';
 import { ProjectApiService } from 'src/app/user/service/project-api.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'fl-work-in-progress',
@@ -10,10 +11,12 @@ import { ProjectApiService } from 'src/app/user/service/project-api.service';
 })
 export class WorkInProgressComponent {
   userId: number = 0;
+
   selectedStatus: String[] = [ProjectStatus.IN_PROGRESS];
 
   constructor(
     private projectApiService: ProjectApiService,
+    private route: ActivatedRoute,
     private router: Router
   ) {
     this.userId = Number(localStorage.getItem('userId'));
@@ -25,6 +28,32 @@ export class WorkInProgressComponent {
     this.projects = await this.projectApiService.getProjects({
       clientId: this.userId,
       status: this.selectedStatus,
+    });
+  }
+  async completeProject(projectId:number) {
+    Swal.fire({
+      title: 'Complete this project?',
+      text: 'This project will be finished and completed.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#14b8a6',
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+    }).then(async (result) => {
+      if (result.value) {
+        const response = await this.projectApiService.updateProject({ projectId: projectId, projectStatus: ProjectStatus.COMPLETED.toString() });
+        if (response != null) {
+          Swal.fire({
+            title: 'Approved',
+            text: 'You can see this project in "Past Project" section',
+            icon: 'success',
+            confirmButtonColor: '#14b8a6',
+            confirmButtonText: 'Ok',
+          }).then(() => {
+            this.router.navigateByUrl('/home/myProjects/client/past');
+          });
+        }
+      }
     });
   }
 }
